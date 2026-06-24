7ad54add43f7365399c8373a7252a58c2bea2141
Miguel Braga Silva <1115241@senacminas.edu.br>
2026-06-24 15:20:31 -0300

    v.1.2.2

--- natação/backend/server.js
+++ natação/backend/server.js
@@ -7,10 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password : "",
-    database : "natacao"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "natacao",
+    port: process.env.DB_PORT || 3306,
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : null
 });
 
 db.connect((erro) => {
@@ -20,9 +22,26 @@ db.connect((erro) => {
         return;
     }
     console.log("Conectado com sucesso");
+    const criarTabelaSQL = `
+    CREATE TABLE IF NOT EXISTS alunos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        idade INT NOT NULL,
+        telefone VARCHAR(20) NOT NULL,
+        nivel VARCHAR(50) NOT NULL,
+        horario VARCHAR(50) NOT NULL,
+        ativo BOOLEAN DEFAULT TRUE
+    )
+`;
+
+    db.query(criarTabelaSQL, (erroTabela) => {
+        if (erroTabela) {
+            console.log("Erro ao criar tabela", erroTabela);
+        } else{
+        console.log("Tabela criada com sucesso, Pronto para uso");
+        }
+    });
 });
-
-
 
 app.get("/", (req, res) => { //mostrar informacao
     res.json({
@@ -172,9 +191,9 @@ app.post ("/admin", (req,res) => {
 });
 
 
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log("Servidor rodando em:")
-    console.log("http://localhost:3000")
+    console.log(`Porta ${PORT}`)
 })
 
--- natação/frontend/script.js
+++ natação/frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000"
+const API = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://miguel-natacao.onrender.com";
 async function cadastrarAluno(event) {
     event.preventDefault();
     const nome = document.getElementById("nome").value;
